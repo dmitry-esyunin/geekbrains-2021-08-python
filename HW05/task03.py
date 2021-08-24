da37c73efd2092cf0a6b38387bc12a979b4f1fd6
Dmitry Esyunin <74874618+dmitry-esyunin@users.noreply.github.com>
2021-08-24 14:04:23 +0300

    Update task03.py
    Забыл про среднюю зарплату в 3-м задании...

--- HW05/task03.py
+++ HW05/task03.py
@@ -11,14 +11,20 @@ n = 20000
 file = 'task03.txt'
 separator = '\t'
 employees = []
-
+num_employees = 0
+sum_ = 0
 with open(file, encoding='utf-8') as f:
     for line in f:
         employee = line.split(separator)
         if len(employee) > 1:
-            if int(employee[1]) < n:
+            salary = int(employee[1])
+            num_employees += 1
+            sum_ += salary
+            if salary < n:
                 employees.append(employee[0])
 
+if num_employees > 0:
+    print(f'Средняя зарплата: {sum_ / num_employees}')
 print(employees)
 
 
